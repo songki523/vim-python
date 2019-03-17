a32f30f05097e7e59f04fcf7b498c88b165adf8b
FOBRider <songki523@gmail.com>
2019-03-16 22:55:17 -0400

    Update git-clone

--- vim-python.sh
+++ vim-python.sh
@@ -1,6 +1,6 @@
 sudo apt-get update
 sudo apt-get install python3-dev
-echo 'clone form git repo'
+git clone https://github.com/vim/vim.git
 cd vim/src
 ./configure --enable-python3interp --with-features=huge --prefix=$HOME/opt/vim --with-python3-config-dir=/usr/lib/python3.5/config-3.5m-x86_64-linux-gnu --enable-fail-if-missing
 make && make install
